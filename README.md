12a6224b3dd4f5080cf5b1a6d10467dd4a07c4a7
Scott McAndrew <scottymcandrew@outlook.com>
2020-09-22 11:49:29 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # prisma-cloud-demos
+
+This repository will be a collection of specific Prisma Cloud demos based on common use cases i.e. scenario-based.
+
---
+++ cwpp/aks-sock-shop-onboarding/main.tf
@@ -0,0 +1,43 @@
+# This script implies Service Principal or interactive AZ Logon before script invocation.
+# SP is required if running from CI/CD. If using this method, set ENV as below
+
+## export ARM_CLIENT_ID="00000000-0000-0000-0000-000000000000"
+## export ARM_CLIENT_SECRET="00000000-0000-0000-0000-000000000000"
+## export ARM_SUBSCRIPTION_ID="00000000-0000-0000-0000-000000000000"
+## export ARM_TENANT_ID="00000000-0000-0000-0000-000000000000"
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg_aks" {
+  location = "West Europe"
+  name = "RG_aks-sock-shop-demo"
+}
+
+resource "azurerm_kubernetes_cluster" "aks-cluster" {
+  dns_prefix = "aks-sock-shop-demo"
+  location = azurerm_kubernetes_cluster
+  name = "aks-sock-shop-demo"
+  resource_group_name = azurerm_resource_group
+
+  default_node_pool {
+    name = "aks-pool"
+    vm_size = "Standard_D2_v2"
+    node_count = 1
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  tags = {
+    Environment = "Lab"
+  }
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks-cluster.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks-cluster.kube_config_raw
+}
